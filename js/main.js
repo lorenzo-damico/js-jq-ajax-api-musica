7222d52066490bd2ade52999fa9a6d8d5dfc0456
lorenzo-damico <loredam@hotmail.it>
2020-09-17 15:43:52 +0200

    Aggiunti dischi con handlebars

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+// Facciamo una chiamata ajax all'api di boolean al seguente indirizzo.
+// https://flynn.boolean.careers/exercises/api/array/music
+// L'api ci restituirà decina di dischi musicali che dovremo stampare a schermo con Handlebars.
+// Concentratevi sulla parte JS per la grafica potrete utilizzare il layout che troverete al seguente link
+// https://bitbucket.org/booleancareers/ex-dischi-musicali-layout/downloads/
+// Bonus:
+// Creare una select con i seguenti generi: pop, rock, metal e jazz.
+// In base a cosa scegliamo nella select vedremo solo i corrispondenti cd.
+
 $(document).ready(function() {
-	//Code
+
+  // Effettuo una chiamata ajax all'api di boolean.
+  $.ajax(
+    {
+      "url": "https://flynn.boolean.careers/exercises/api/array/music",
+      "method": "GET",
+      "success": function (data, stato) {
+
+        // Salvo la lista dei dischi ottenuta in un array dischi.
+        var dischi = data.response;
+        console.log(dischi);
+
+        // Utilizziamo handlebars
+        var source = $("#disco-template").html();
+        var template = Handlebars.compile(source);
+
+        // Imposto un ciclo per stampare con handlebars tutti gli oggetti disco contenuti nell'array dischi.
+        for (var i = 0; i < dischi.length; i++) {
+          var html = template(dischi[i]);
+          $(".cds-container").append(html);
+        }
+
+      },
+      "error": function (richiesta, stato, errori) {
+        console.log(stato);
+      }
+    }
+  );
+
+
+
+
+
+
 });
